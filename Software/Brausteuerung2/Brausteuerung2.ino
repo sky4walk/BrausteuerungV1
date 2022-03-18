7645735ac7c8ba41e052e7e93fab383909da19c9
Andre Betz <git@andrebetz.de>
2022-03-18 18:30:13 +0100

    started menue

--- Software/Brausteuerung2/Brausteuerung2.ino
+++ Software/Brausteuerung2/Brausteuerung2.ino
@@ -39,6 +39,9 @@ typedef struct
 {
   unsigned long  time;
   int temp;
+  bool active;
+  bool wait;
+  bool alarm;
 } Braurast;
 struct Rezept
 {
@@ -55,7 +58,8 @@ struct Rezept
   int SwitchBits;
   unsigned long SwitchOn;
   unsigned long SwitchOff;
-  int LastState;
+  int actRast;
+  bool started;
   bool UseDefault;
 } myRezept;
 ///////////////////////////////////////////////////////////////////////////////
@@ -65,6 +69,9 @@ double pidOutput                 = 0;
 double isTmp                     = 25;
 double sollTmp                   = 25;
 bool   btnPressed[MAXBUTTONS]    = {false,false,false,false,false,false};
+bool   heatState                 = false;
+int    actMenuState              = 0;
+int    subMenuState              = 0;
 ///////////////////////////////////////////////////////////////////////////////
 // classes
 ///////////////////////////////////////////////////////////////////////////////
@@ -76,6 +83,7 @@ RCSwitch          mySwitch = RCSwitch();
 PID myPID(&isTmp, &pidOutput, &sollTmp, myRezept.pidKp,myRezept.pidKi,myRezept.pidKd, DIRECT);
 WaitTime          timerTempMeasure;
 WaitTime          timerPidCompute;
+WaitTime          timerSendHeatState;
 StorageEEProm     store;
 ///////////////////////////////////////////////////////////////////////////////
 // setDefaultValues
@@ -89,16 +97,20 @@ void LoadValues()
   {
     // only go here after first installation
     memset((char*)&myRezept, 0, sizeof(Rezept));
+    myRezept.pidKp             = 0.0f;
+    myRezept.pidKi             = 0.0f;
+    myRezept.pidKd             = 0.0f;
     myRezept.PidOWinterval     = PIDOWINTERVAL;
     myRezept.PidWindowSize     = PIDWINDOWSIZE;
     myRezept.PidMinWindow      = PIDMINWINDOW;
-    myRezept.SwitchOn          = 1631343;//1631343;
-    myRezept.SwitchOff         = 1631342;//1631342;
+    myRezept.SwitchOn          = 1631343;
+    myRezept.SwitchOff         = 1631342;
     myRezept.SwitchProtocol    = 1;
     myRezept.SwitchPulseLength = 315;
     myRezept.SwitchBits        = 24;
     myRezept.SwitchRepeat      = 15;
-    myRezept.LastState         = 0;
+    myRezept.actRast           = 0;
+    myRezept.started           = false;
     myRezept.UseDefault        = true;
     store.save(0, sizeof(Rezept), (char*)&myRezept);
   }
@@ -108,14 +120,25 @@ void LoadValues()
 ///////////////////////////////////////////////////////////////////////////////
 void setButton(int btnNr) 
 {
-  if ( MAXBUTTONS > btnNr ) 
+  if ( MAXBUTTONS > btnNr  && 0 != btnNr ) 
+  {
     if ( false == btnPressed[btnNr] )
+    {
       btnPressed[btnNr] = true;
+      CONSOLE("b");
+      CONSOLELN(btnNr);
+    }
+  }
 }
-void resetButton(int btnNr)
+bool isButtonPressed(int btnNr)
 {
+  bool pressed = false;
   if ( MAXBUTTONS > btnNr )
+  {
+    pressed = btnPressed[btnNr];
     btnPressed[btnNr] = false;
+  }
+  return pressed;
 }
 int Keypad()
 {
@@ -139,7 +162,7 @@ int Keypad()
   while ( val != val_old );
 
   setButton(val);
-  
+  if ( 0 < val ) delay(200);
   return val;
 }
 ///////////////////////////////////////////////////////////////////////////////
@@ -149,9 +172,75 @@ void printLogo()
 {
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Brauverein.org");
+  lcd.print(F("Brauverein.org"));
   delay(2000);
   lcd.clear();
+}
+///////////////////////////////////////////////////////////////////////////////
+// menubuttons
+///////////////////////////////////////////////////////////////////////////////
+bool SubStateChange(int maxStates)
+{
+    if ( isButtonPressed (btnUP) ) 
+    {
+      CONSOLE(F("Up"));
+      CONSOLELN(subMenuState);
+      subMenuState++;
+      if ( subMenuState == maxStates )
+        subMenuState = 0;
+    }
+    else if ( isButtonPressed (btnDOWN) ) 
+    { 
+      CONSOLE(F("Down"));
+      CONSOLELN(subMenuState);
+      if ( 0 == subMenuState )
+      {
+        subMenuState = maxStates-1;
+      }      
+      else 
+      {
+        subMenuState--;
+      }
+    }
+    else if ( isButtonPressed (btnLEFT) ) 
+    { 
+      return true;
+    }
+    return false;
+}
+///////////////////////////////////////////////////////////////////////////////
+// menu
+///////////////////////////////////////////////////////////////////////////////
+void menu()
+{
+  lcd.setCursor(0, 0);
+  switch(actMenuState)
+  {
+    case 0:
+    {
+      if ( true == SubStateChange(2) )
+      {        
+        if ( 0 == subMenuState )
+          actMenuState = 1;
+        else 
+          actMenuState = 2;
+        CONSOLE(F("S"));
+        CONSOLELN(actMenuState);
+      }
+      lcd.setCursor(0, 0);
+      lcd.print(F("Menu"));
+      lcd.setCursor(0, 1);
+      if ( 0 == subMenuState )
+      {
+        lcd.print(F("Brew"));
+      } 
+      else
+      {
+        lcd.print(F("Set "));
+      }
+      break;
+    }
+  }
 }
 ///////////////////////////////////////////////////////////////////////////////
 // setup
@@ -163,11 +252,13 @@ void setup()
   
   // load and set values
   LoadValues();
-    
+
+  // set some HW Pins  
   pinMode(PINTMPDS18B20,  INPUT);
   pinMode(PINBUZZER,      OUTPUT);
   pinMode(PINLCD,         OUTPUT);
   analogWrite (PINBUZZER, 0);
+  digitalWrite(PINLCD, HIGH);
 
   // Start up DS18B20
   sensors.begin();
@@ -175,28 +266,30 @@ void setup()
   sensors.setResolution(tempDeviceAddress, TEMPRESOLUTION);
   sensors.setWaitForConversion(false);
   sensors.requestTemperatures();
+  
   // start 433MHz modul switch
-
   mySwitch.enableTransmit(PINHEATER); //D13
   mySwitch.setProtocol(myRezept.SwitchProtocol);
   mySwitch.setPulseLength(myRezept.SwitchPulseLength);
   mySwitch.setRepeatTransmit(myRezept.SwitchRepeat); 
   
-  digitalWrite(PINLCD, HIGH);
-
+  // configure timer
   timerTempMeasure.setTime(myRezept.PidOWinterval);
-  timerPidCompute.setTime(myRezept.PidWindowSize); 
-  delay(500);
+  timerPidCompute.setTime(myRezept.PidWindowSize);
+  timerSendHeatState.setTime(myRezept.PidWindowSize);
+  
+  // logo
   printLogo();
 }
 ///////////////////////////////////////////////////////////////////////////////
 // loop
 ///////////////////////////////////////////////////////////////////////////////
-bool switchon = false;
 void loop ()
 {
-  int keyState = Keypad();
-
+  // read key pressed
+  Keypad();
+  // show menu
+  menu();
   // temperatur meassure
   timerTempMeasure.start();
   if ( timerTempMeasure.timeOver() )
@@ -212,9 +305,7 @@ void loop ()
     else
     {
       CONSOLELN(isTmp);
-    }   
-    
-  
+    }         
   }
 
   // PID compute 
@@ -222,18 +313,22 @@ void loop ()
   if ( timerPidCompute.timeOver() )
   {
     timerPidCompute.restart();
+  }
 
-    if ( switchon )
+  // always send state
+  timerSendHeatState.start();
+  if ( timerSendHeatState.timeOver() )
+  {
+    timerSendHeatState.restart();
+    if ( heatState )
     {
         CONSOLELN("On");
         mySwitch.send(myRezept.SwitchOn,myRezept.SwitchBits);
-        switchon = false;
     }
     else
     {
         CONSOLELN("Off");
         mySwitch.send(myRezept.SwitchOff,myRezept.SwitchBits);
-        switchon = true;
     } 
   }
 }
